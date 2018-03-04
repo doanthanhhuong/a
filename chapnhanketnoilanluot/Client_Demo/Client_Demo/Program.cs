30a8f12287fa9695e477d68f84d3fdc6d2de4b7e
doanthanhhuong <huongthanhlan@gmail.com>
2018-03-04 16:41:20 +0700

    hello

---
+++ chapnhanketnoilanluot/Client_Demo/Client_Demo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Demo
+{
+    class ClientDemo
+    {
+        private TcpClient _client;
+        private Boolean _isConnected;
+
+        public ClientDemo(String ipAddress, int portNum)
+        {
+            _client = new TcpClient();
+            _client.Connect(ipAddress, portNum);
+            HandleCommunication();
+        }
+        public void HandleCommunication()
+        {
+            //_sReader = new StreamReader(_client.GetStream(), Encoding.ASCII);
+            //_sWriter = new StreamWriter(_client.GetStream(), Encoding.ASCII);
+            NetworkStream stream = _client.GetStream();
+            _isConnected = true;
+            String sData = null;
+            Byte[] bytes = new Byte[256];
+            int i;
+            while (_isConnected)
+            {
+                // repuest
+                Console.Write("Client 1 :");
+                sData = Console.ReadLine();
+                // send
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(sData);
+                stream.Write(msg,0,msg.Length);
+                // reponse
+                i = stream.Read(bytes, 0, bytes.Length);
+                string str = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Server :"+str);
+                if (str.Equals("bye"))
+                {
+                    Console.ReadKey();
+                    break;
+                }
+                   
+            }
+            stream.Close();
+            _client.Close();
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mutil Threaded TCP Server Demo");
+            Console.WriteLine("Provide TCP: ");
+            String ip = Console.ReadLine();
+            Console.WriteLine("Provide Port:");
+            int port = Int32.Parse(Console.ReadLine());
+            ClientDemo client = new ClientDemo(ip, port);
+        }
+    }
+}
---
+++ chapnhanketnoilanluot/Server_Demo/Server_Demo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server_Demo
+{
+    class TcpServer
+    {
+        private TcpListener _server;
+        private int count = 0;
+
+        public TcpServer(int port)
+        {
+            _server = new TcpListener(IPAddress.Any, port);
+            _server.Start();
+            if(count<10)
+            LoopClients();
+        }
+        public void LoopClients()
+        {
+
+            // wait for  client connection
+               count++;
+                TcpClient newclient = _server.AcceptTcpClient();
+            // client found
+            // create a thread to handle commuication                
+            Thread t = new Thread((obj) => { HandleClient((TcpClient)obj); });
+            t.Start(newclient);
+        }
+        public void HandleClient( Object obj)
+        {
+            // retrieve client from paramater pass to thread
+            TcpClient client = (TcpClient)obj;
+            // sets two streams
+            NetworkStream stream = client.GetStream();
+            
+            // you could use the NetworkSteam to read and write
+            // but there is no forcing flush, even when requested
+            Console.WriteLine("Connected !");
+            String sData = null;
+         //   Boolean bClientConnected = true;
+            Byte[] bytes = new Byte[256];
+        
+            while (client.Connected)
+            {
+                // read 
+                int i = stream.Read(bytes, 0, bytes.Length);
+             
+                sData = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Client :" + sData);
+                // send
+                byte[] a;
+                if (sData.Equals("exit"))
+                {
+                    a = System.Text.Encoding.ASCII.GetBytes("bye");
+                    stream.Write(a, 0, a.Length);
+                    LoopClients();
+                    break;
+                }
+                
+                 a = System.Text.Encoding.ASCII.GetBytes("Dong y"+count);
+                stream.Write(a, 0, a.Length);        
+            }
+            stream.Close();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mutil Threaded TCP Server Demo:");
+            TcpServer server = new TcpServer(5555);
+        }
+    }
+}
